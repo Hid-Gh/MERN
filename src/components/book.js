115217c970c403d021083a59ea5b3df3d800a51c
Hidaya-Gh <hidaya.gharad.2002@gmail.com>
2023-06-08 09:40:44 +0300

    other folders

---
+++ src/components/book.js
@@ -0,0 +1,52 @@
+export const bookings =[
+            {
+              "id": 1,
+              "destination": {
+                "name": "New York",
+                "currency": "USD"
+              },
+              "cost": 500,
+              "flightDate": "2023-06-15T08:00:00.000Z",
+              "returnDate": "2023-06-20T08:00:00.000Z"
+            },
+            {
+              "id": 2,
+              "destination": {
+                "name": "Paris",
+                "currency": "EUR"
+              },
+              "cost": 700,
+              "flightDate": "2023-07-01T08:00:00.000Z",
+              "returnDate": "2023-07-10T08:00:00.000Z"
+            },
+            {
+              "id": 3,
+              "destination": {
+                "name": "Tokyo",
+                "currency": "JPY"
+              },
+              "cost": 1000,
+              "flightDate": "2023-07-15T08:00:00.000Z",
+              "returnDate": "2023-07-25T08:00:00.000Z"
+            },
+            {
+              "id": 4,
+              "destination": {
+                "name": "Sydney",
+                "currency": "AUD"
+              },
+              "cost": 900,
+              "flightDate": "2023-08-01T08:00:00.000Z",
+              "returnDate": "2023-08-10T08:00:00.000Z"
+            },
+            {
+              "id": 5,
+              "destination": {
+                "name": "Cancun",
+                "currency": "MXN"
+              },
+              "cost": 600,
+              "flightDate": "2023-08-15T08:00:00.000Z",
+              "returnDate": "2023-08-20T08:00:00.000Z"
+            }
+          ]
